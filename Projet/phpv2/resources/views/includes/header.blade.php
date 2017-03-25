ff68cf11942e0ceb4cb8a7419471aabfb0012152
v_reeb <victor.reeb@live.fr>
2017-03-25 12:47:26 +0100

    fixes route home deleted

--- Projet/phpv2/resources/views/includes/header.blade.php
+++ Projet/phpv2/resources/views/includes/header.blade.php
@@ -4,7 +4,7 @@
 		<a href="#" class="brand-logo">PHPv2</a>
 		<ul id="nav-mobile" class="right hide-on-med-and-down">
 				@if (Auth::check())
-					<li><a href="{{ url('/home') }}">Home</a></li>
+					<li><a href="{{ url('/') }}">Home</a></li>
 					<li><a href="#">Déconnexion</a></li>
 				@else
 					<li><a href="{{ url('/login') }}">Login</a></li>
