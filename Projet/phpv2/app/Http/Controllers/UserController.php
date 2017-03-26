204a8104840396f68471cb627d7240c08ea0ff90
Pi <pierrebecker57540@gmail.com>
2017-03-26 16:35:04 +0200

    UserController

--- Projet/phpv2/app/Http/Controllers/UserController.php
+++ Projet/phpv2/app/Http/Controllers/UserController.php
@@ -17,15 +17,35 @@ class UserController extends Controller
 
     public function update_avatar(Request $request){
 
-    	// Handle the user upload of avatar
-    	if($request->hasFile('avatar')){
-    		$avatar = $request->file('avatar');
-    		$filename = time() . '.' . $avatar->getClientOriginalExtension();
-    		Image::make($avatar)->resize(300, 300)->save( public_path('/uploads/avatars/' . $filename ) );
-    		$user = Auth::user();
-    		$user->avatar = $filename;
-    		$user->save();
-    	}
-    	return view('auth/profile', array('user' => Auth::user()));
+    	// // Handle the user upload of avatar
+    	// if($request->hasFile('avatar')){
+    	// 	$avatar = $request->file('avatar');
+    	// 	$filename = time() . '.' . $avatar->getClientOriginalExtension();
+    	// 	$img = Image::make($avatar)->resize(300, 300)->save( public_path('/uploads/avatars/' . $filename ) );
+    	// 	$user = Auth::user();
+    	// 	$user->avatar = $filename;
+    	// 	$user->save();
+    	// }
+    	// return view('auth/profile', array('user' => Auth::user()));
+      $image = new Image();
+      $this->validate($request, [
+          'title' => 'required',
+          'image' => 'required'
+      ]);
+      $image->title = $request->title;
+      $image->description = $request->description;
+      if($request->hasFile('image')) {
+          $file = Input::file('image');
+          //getting timestamp
+          $timestamp = str_replace([' ', ':'], '-', Carbon::now()->toDateTimeString());
+
+          $name = $timestamp. '-' .$file->getClientOriginalName();
+
+          $image->filePath = $name;
+
+          $file->move(public_path().'/images/', $name);
+      }
+      $image->save();
+      return $this->create()->with('success', 'Image Uploaded Successfully');
     }
 }
--- Projet/phpv2/resources/views/auth/profile.blade.php
+++ Projet/phpv2/resources/views/auth/profile.blade.php
@@ -10,7 +10,7 @@
             <form enctype="multipart/form-data" action="{{ url('profile') }}" method="POST">
                 <label>Update Profile Image</label>
                 <input type="file" name="avatar">
-                <input type="hidden" name="_token" value="{{ csrf_token() }}">
+                <input type="hidden" name="_token" value="{{ csrf_field() }}">
                 <input type="submit" class="pull-right btn btn-sm btn-primary">
             </form>
         </div>
