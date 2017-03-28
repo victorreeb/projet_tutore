cc35268c8f8552984b6382c0822b013214d43c16
Pi <pierrebecker57540@gmail.com>
2017-03-28 19:22:34 +0200

    Fix Views

--- phpv2/resources/views/dashboard/groupes/create.blade.php
+++ phpv2/resources/views/dashboard/groupes/create.blade.php
@@ -11,12 +11,12 @@
     <form class="form-horizontal" role="form" method="POST" action="{{ route('groupe.create') }}">
       {{ csrf_field() }}
       <div class="row">
-        <div class="input-field col s12">
+        <div class="input-field col s12 center">
           <input type="text" id="name" name="name" class="materialize-textarea"></input>
           <label for="name">Nom</label>
-        </div>
-      </div>
-      <button type="submit" class="btn btn-primary">Créer</button>
+      <button type="submit" class="btn waves-effect orange white-text text-accent-2">Créer</button>
+    </div>
+    </div>
     </form>
   </div>
 </div>
